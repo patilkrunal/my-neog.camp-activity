1526aa8818f9db67df53b540081b2118e5871bc8
Krunal Patil <47073116+patilkrunal@users.noreply.github.com>
2021-09-08 11:14:22 +0000

    exercise-2 done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Krunal Patil');
+var readlineSync = require('readline-sync');
+
+var username = readlineSync.question('Enter your name: ');
+
+console.log('Hi ' + username + '. This is a short quiz.');
